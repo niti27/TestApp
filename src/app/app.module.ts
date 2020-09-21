13b06ce42980469f3076a0b77ac83e3c6ec0f333
Niti Wadji <niti@infi-pre.com>
2020-09-21 20:25:35 +0530

    added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    DashboardComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
